fb75e63ee13fb0d92c17d312065a935746498b30
fbma <fbma@cin.ufpe.br>
2020-05-18 16:58:46 -0300

    Final do primeiro modulo

---
+++ python-5/main.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+records = [
+    {'source': '48-996355555', 'destination': '48-666666666',
+        'end': 1564610974, 'start': 1564610674},
+    {'source': '41-885633788', 'destination': '41-886383097',
+        'end': 1564506121, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-886383097',
+        'end': 1564630198, 'start': 1564629838},
+    {'source': '48-999999999', 'destination': '41-885633788',
+        'end': 1564697158, 'start': 1564696258},
+    {'source': '41-833333333', 'destination': '41-885633788',
+        'end': 1564707276, 'start': 1564704317},
+    {'source': '41-886383097', 'destination': '48-996384099',
+        'end': 1564505621, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '48-996383697',
+        'end': 1564505721, 'start': 1564504821},
+    {'source': '41-885633788', 'destination': '48-996384099',
+        'end': 1564505721, 'start': 1564504821},
+    {'source': '48-996355555', 'destination': '48-996383697',
+        'end': 1564505821, 'start': 1564504821},
+    {'source': '48-999999999', 'destination': '41-886383097',
+        'end': 1564610750, 'start': 1564610150},
+    {'source': '48-996383697', 'destination': '41-885633788',
+        'end': 1564505021, 'start': 1564504821},
+    {'source': '48-996383697', 'destination': '41-885633788',
+        'end': 1564627800, 'start': 1564626000}
+]
+
+
+def get_price(call):
+    price = 0.36
+    for time in range(call['start']+60, call['end']+1, 60):
+        hour = datetime.fromtimestamp(time).hour
+        if hour >= 6 and hour <= 22:
+            price = price + 0.09
+    return price
+
+
+def classify_by_phone_number(records):
+    prices = {}
+    result = []
+    for call in records:
+        source = call['source']
+        price = get_price(call)
+        if source in prices:
+            prices[source] += price
+        else:
+            prices[source] = price
+
+    for source in prices:
+        result.append({'source': source, 'total': round(prices[source], 2)})
+
+    result.sort(reverse=True, key=lambda i: i['total'])
+
+    return result
+    pass
